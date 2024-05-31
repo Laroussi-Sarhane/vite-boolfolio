d441508a1c9cf2b4f5820945874a82010ee96f34
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-31 16:53:25 +0200

    add files and api

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 
 
 <script>
+import axios from 'axios';
 export default {
-
+methods:{
+  getApi(){
+    console.log('get Api');
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then(result=>{
+      console.log(result.data);
+    })
+    .catch(error=>{
+      console.log(error);
+    })
+  }
+},
+mounted(){
+  this.getApi()
+}
 }
 </script>
 
 <template>
+ <div class="main-wrapper">
   <h1>vite-boolfolio</h1>
+ </div>
   
 </template>
 

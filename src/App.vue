a7f37df261798d4b976dfea4176af1b751eb14b3
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-05-31 17:05:59 +0200

    add changes

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,19 @@
 <script>
 import axios from 'axios';
 export default {
+  data(){
+    return{
+      'posts':[],
+    }
+
+  },
 methods:{
   getApi(){
     console.log('get Api');
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then(result=>{
-      console.log(result.data);
+      this.posts= result.data
+      console.log(this.posts);
     })
     .catch(error=>{
       console.log(error);
@@ -24,6 +31,14 @@ mounted(){
 <template>
  <div class="main-wrapper">
   <h1>vite-boolfolio</h1>
+  <h2>i miei post </h2>
+  <div class="">
+    <ul>
+    <li v-for="post in posts" :key="post.response">{{ post.response }}</li>
+    
+
+  </ul>
+  </div>
  </div>
   
 </template>

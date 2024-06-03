3279b3fbbbf70f294a5fa9e8c0a3187b1210a580
Laroussi Sarhane <sarhanelaroussi2@gmail.com>
2024-06-03 17:11:40 +0200

    add pages

---
+++ src/pages/about.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+name:'about'
+}
+</script>
+<template>
+<h1>about </h1>
+  <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptates, sequi officia! Illum aliquam et dicta modi, consequatur ullam culpa minus consequuntur libero exercitationem esse alias tempora rem. Ad, architecto reprehenderit.
+  </p>
+</template>
+
+
+<style>
+
+</style>
